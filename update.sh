c7816f62a4ca96802a2be9f2981069d1df8fc619
Jonathan Cole <joncole04412@gmail.com>
2019-07-31 17:39:55 -0400

    Fix bugs

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 
 version="1.0.0"
-dest="bin/override_ios_simulator_status-$version.tar.gz"
-tar -cvzf $dest override_ios_simulator_status
+dest="override_ios_simulator_status-$version.tar.gz"
+tar --exclude='./gitignore' --exclude='./DS_Store' --exclude='./.git' --exclude='./readme.md' --exclude='./LICENSE.md' --exclude='./update.sh' --exclude='./*.tar.gz' -zcvf "$dest" .
 shasum -a 256 $dest
