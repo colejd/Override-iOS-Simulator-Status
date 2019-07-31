a6bac40776580e3b3e7a51cdbe20462158180873
Jonathan Cole <joncole04412@gmail.com>
2019-07-31 18:01:24 -0400

    Update to 1.0.1 (remove default args when none are supplied)

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 
-version="1.0.0"
+version="1.0.1"
 dest="override_ios_simulator_status-$version.tar.gz"
-tar --exclude='./gitignore' --exclude='./DS_Store' --exclude='./.git' --exclude='./readme.md' --exclude='./LICENSE.md' --exclude='./update.sh' --exclude='./*.tar.gz' -zcvf "$dest" .
+tar --exclude='./.gitignore' --exclude='./.DS_Store' --exclude='./.git' --exclude='./update.sh' --exclude='./*.tar.gz' -zcvf "$dest" .
 shasum -a 256 $dest
